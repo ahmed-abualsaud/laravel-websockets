64931e11f281661cc25f458b086e2069efe734fc
sicoman <ahmedtheseer891996@gmail.com>
2021-12-23 17:23:48 +0200

    set ssl configurations

--- config/websockets.php
+++ config/websockets.php
@@ -115,20 +115,18 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => '/etc/nginx/ssl/ws.qruz.xyz/1281036/server.crt',
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => '/etc/nginx/ssl/ws.qruz.xyz/1281036/server.key',
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
 
         /*
          * Passphrase for your local_cert file.
          */
         'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
-
-        'verify_peer' => false,
 
     ],
 

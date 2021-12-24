6203c8f4a55eb08f212418f546033b325b7e4d2b
sicoman <ahmedtheseer891996@gmail.com>
2021-12-24 17:04:17 +0200

    set pusher options

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,14 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'encrypted' => true,
+                'host' => 'ws.qruz.xyz',
+                'port' => 443,
+                'scheme' => env('PUSHER_APP_SCHEME'),
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
             ],
         ],
 

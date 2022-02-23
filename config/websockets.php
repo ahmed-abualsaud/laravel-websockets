c2ab415362e07c78d17eb23b3f8cc304590fcb36
sicoman <ahmedtheseer891996@gmail.com>
2022-02-23 13:30:54 +0200

    enable client messages

--- config/websockets.php
+++ config/websockets.php
@@ -29,7 +29,7 @@ return [
             'secret' => env('STAGING_APP_SECRET'),
             'path' => env('STAGING_APP_PATH'),
             'capacity' => null,
-            'enable_client_messages' => false,
+            'enable_client_messages' => true,
             'enable_statistics' => true,
         ],
     ],

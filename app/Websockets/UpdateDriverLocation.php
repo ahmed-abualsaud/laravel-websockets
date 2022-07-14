adb1532c7c21ba29b8e96aaf677677d949b31691
sicoman <ahmedtheseer891996@gmail.com>
2022-07-14 12:25:18 +0200

    access database from websocket

--- app/Websockets/UpdateDriverLocation.php
+++ app/Websockets/UpdateDriverLocation.php
@@ -24,10 +24,14 @@ class UpdateDriverLocation extends WebSocketHandler
             array_key_exists('data', $decoded_message) && $decoded_message['data']) {
 
             $args = json_decode($decoded_message['data'], true);
-            Driver::where('id', $args['driver_id'])->update([
-                'latitude' => $args['latitude'],
-                'longitude' => $args['longitude']
-            ]);
+            $channel_data = explode('.', $decoded_message['channel']);
+            if (in_array($channel_data[0], ['private-Update', 'Update']) && $channel_data[1] == 'Driver' && $channel_data[2] == 'Location')
+            {
+                Driver::where('id', $channel_data[3])->update([
+                    'latitude' => $args['latitude'],
+                    'longitude' => $args['longitude']
+                ]);
+            }
 
             if (array_key_exists('request_id', $args) && $args['request_id']) {
                 $input = Arr::only($args, ['request_id', 'latitude', 'longitude']);
